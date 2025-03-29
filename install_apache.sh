76ee411ffd4d575d7c55d3f507ff9a5def564aac
suren-grg12 <gorkhalitamu25@gmail.com>
2025-03-29 10:28:30 +0000

    adding if condition to check apache2

--- install_apache.sh
+++ install_apache.sh
@@ -2,6 +2,12 @@
 echo"installing apache 2"
 sudo apt update
 sudo apt install apache2
-sudo systemctl start apache2
-sudo systemctl enable apache2
-sudo systemctl status apache2
+if [ -f /var/run/apache2.pid];
+then 
+	echo "apache2 is running"
+else
+	echo "apache2 is not running"
+	sudo systemctl start apache2
+fi
+
+
