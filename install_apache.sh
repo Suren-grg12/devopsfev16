354e42445cab57b43032096e1a2be736a61cef94
suren-grg12 <gorkhalitamu25@gmail.com>
2025-03-29 15:58:38 +0000

    installing packages of dockers

--- install_apache.sh
+++ install_apache.sh
@@ -11,4 +11,14 @@ else
 	sudo systemctl eanble apache2
 fi
 
+echo "installing  docker "
 
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+echo "adding docker official GPG key"
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
